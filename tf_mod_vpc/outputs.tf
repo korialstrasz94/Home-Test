6fee35d0ff3252c7ef4d81989674bfc62e5550d2
rt <korialstrasz94@gmail.com>
2025-12-12 21:34:55 +0100

    Uploading the first files.

---
+++ tf_mod_vpc/outputs.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnets" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway ids"
+  value       = aws_nat_gateway.nat.*.id
+}
+
+output "s3_vpc_endpoint_id" {
+  description = "VPC endpoint id for S3"
+  value       = aws_vpc_endpoint.s3.id
+}
