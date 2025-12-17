4ff5f3384c329d18630149cd24a6ee34f9722397
rt <korialstrasz94@gmail.com>
2025-12-17 11:09:29 +0100

    Adding description to outputs.

--- tf_mod_vpc/outputs.tf
+++ tf_mod_vpc/outputs.tf
@@ -1,12 +1,15 @@
 output "vpc_id" {
+  description = "The ID of the newly created VPC"
   value = aws_vpc.main.id
 }
 
 output "public_subnets" {
+  description = "List of public subnet ids"
   value = [for s in aws_subnet.public : s.id]
 }
 
 output "private_subnets" {
+  description = "List of private subnet ids"
   value = [for s in aws_subnet.private : s.id]
 }
 
